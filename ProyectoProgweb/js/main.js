38c2ab26ac5fae4e02a3d019bc3f94e67884236d
Abraham <amg_186@hotmail.com>
2013-10-11 18:57:03 -0500

    ...

--- ProyectoProgweb/js/main.js
+++ ProyectoProgweb/js/main.js
@@ -1,1 +1,27 @@
+var exprnum= /^[0-9]+?/;
 
+$("#buttonlogin").click(function(){
+
+	var $codigo = $("#codigo").val();
+	var $password = $("#contraseña").val(); 
+
+	if($codigo == ""){
+		$("#errorCodigo").replaceWith("<p class=\"errores\" id=\"errorCodigo\">Escribe tu codigo</p>");
+		$("#errorCodigo").fadeIn("slow");
+	}
+	else
+	{
+		$("#errorCodigo").fadeOut();
+		if(!exprNum.test($codigo))
+		{
+			$("#errorCodigo").replaceWith("<p class=\"errores\" id=\"errorCodigo\">Codigo inválido</p>");
+			$("#errorCodigo").fadeIn("slow");
+		}
+	}
+
+	if($password==""){
+		$("#errorPass").fadeIn("slow");
+	}
+}
+
+);
